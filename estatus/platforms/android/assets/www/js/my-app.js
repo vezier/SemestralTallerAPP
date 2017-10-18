fab74729d516a9fab7b0321115d32449c97fda29
vezier <32503545+vezier@users.noreply.github.com>
2017-10-18 03:11:53 -0800

    1

--- estatus/platforms/android/assets/www/js/my-app.js
+++ estatus/platforms/android/assets/www/js/my-app.js
@@ -15,37 +15,76 @@ $$(document).on('deviceready', initapp);
 
 function initapp(){
   console.log("dispositivo listo!!!");
-  $$('#btn').on('click', tomarfoto);
-  $$('#btn1').on('click', tomarfoto1);
+  document.addEventListener("backbutton", onBackKeyDown, false);
+  $$("#rev").on('click',revisa);
+  $$("#logoc").on('click',playAudio);
   }
   
-  function tomarfoto(){
-	  console.log("tomar foto!!");
-	  navigator.camera.getPicture(onSuccess, onFail, options);  
-  }
-  var options ={
-		  sourceType: 1,
-		  quality: 50,
-		  destinationType: 1,
-		  allowEdit: false,
-		  correctOrientation: true,
-		  saveToPhotoAlbum: true
-  }
-  function onSuccess(imageURI){
-	  $$('#foto').attr('src',imageURI);
-  }
-  function onFail(message){
-		console.log('Error!:', message);
-  }
-  function tomarfoto1(){
-	  console.log("tomar foto!!");
-	  navigator.camera.getPicture(onSuccess, onFail, options1);  
-  }
-  var options1 ={
-		  sourceType: 0,
-		  quality: 50,
-		  destinationType: 1,
-		  allowEdit: false,
-		  correctOrientation: true,
-		  saveToPhotoAlbum: true
-  }
+ function onBackKeyDown(){
+			navigator.notification.confirm("Desea salir de la APP?", cerrarAPP,"ADVERTENCIA!", "Si,No");
+ } 
+ function cerrarAPP(e){
+	 if(e==1){
+	  navigator.app.exitApp();
+	 }else{
+		 return;
+	 }
+	 
+	 
+ }
+function playAudio() {
+    // Play the audio file at url
+    var my_media = new Media('http://vzier.u-host.cl/macaco.mp3',
+        // success callback
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio
+    my_media.play();
+}
+function revisa(){
+var u= $("#url").val();
+var p= $("#puerto").val();
+
+
+if(p==""){
+	p=80;
+}
+var dataString="u="+u+"&p="+p;
+if($.trim(u).length>0 & $.trim(p).length>0){
+$.ajax({
+type: "POST",
+url: "https://vzier.000webhostapp.com/we.php",
+data: dataString,
+crossDomain: true,
+cache: false,
+success: function(data){
+	myApp.hidePreloader();
+	if(data=="online"){
+		myApp.hidePreloader();
+		console.log("ONNNNN !!");
+		navigator.vibrate(150);
+		myApp.alert("[ "+u + ":"+p+" ] Se encuentra ONLINE!", "Enhorabuena");
+	}
+	else if(data="offline"){
+		myApp.hidePreloader();
+		console.log("offfofofof !!");
+		navigator.vibrate([100, 50, 100]);
+		myApp.alert("[ "+u+":"+p+" ] No ha respondido.", "Error!");
+	
+	}
+}
+
+});
+}else{
+	myApp.hidePreloader();
+	navigator.vibrate([100, 50, 100]);
+	myApp.alert("Debes Ingresar una URL/IP y un puerto.", "ERROR");
+	
+};
+};
